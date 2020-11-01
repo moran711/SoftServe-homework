3b503596e57158e61a840c395c56a4954ad49f82
Roman <romaniv788@gmail.com>
2020-11-01 17:11:14 +0200

    refactor webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,9 @@
 const path = require('path');
 const webpack = require('webpack');
-const {
-  CleanWebpackPlugin
-} = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
 
 const isProd = process.env.NODE_ENV === 'production';
 const isDev = !isProd;
@@ -14,35 +11,41 @@ const isDev = !isProd;
 console.log('is Prod', isProd);
 console.log('isDev', isDev);
 
-const filename = ext => isDev ? `[name].bundle.${ext}` : `[name].bundle.[hash].${ext}`;
+const filename = (ext) =>
+  isDev ? `[name].bundle.${ext}` : `[name].bundle.[hash].${ext}`;
 
-let htmlPageNames = ['admin', 'best-matches', 'contacts', 'fans', 'news', 'recent-matches'];
+const htmlPageNames = [
+  'admin',
+  'best-matches',
+  'contacts',
+  'fans',
+  'news',
+  'recent-matches',
+];
 
-let getEntry = (pageNames) => {
+const getEntry = (pageNames) => {
   const entry = {};
-  pageNames.forEach(name => {
-    entry[name] = ['@babel/polyfill', `./pages/${name}/${name}.js`]
+  pageNames.forEach((name) => {
+    entry[name] = ['@babel/polyfill', `./pages/${name}/${name}.js`];
   });
   return entry;
-}
+};
 
-let multipleHtmlPlugins = htmlPageNames.map(name => {
+const multipleHtmlPlugins = htmlPageNames.map((name) => {
   return new HTMLWebpackPlugin({
     template: `./pages/${name}/${name}.html`,
     filename: `./pages/${name}/${name}.html`,
-    chunks: [`${name}`]
+    chunks: [`${name}`],
   });
 });
 
 const jsLoaders = () => {
-    const loaders = [
-        'babel-loader'
-    ];
+  const loaders = ['babel-loader'];
 
-    if (isDev) {
-        loaders.push('eslint-loader');
-    }
-    return loaders;
+  if (isDev) {
+    loaders.push('eslint-loader');
+  }
+  return loaders;
 };
 
 module.exports = {
@@ -50,23 +53,23 @@ module.exports = {
   mode: 'development',
   entry: {
     index: ['@babel/polyfill', './index.js'],
-    ...getEntry(htmlPageNames)
+    ...getEntry(htmlPageNames),
   },
   output: {
     filename: filename('js'),
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   resolve: {
     extensions: ['.js'],
     alias: {
       '@': path.resolve(__dirname, 'src'),
-      '@core': path.resolve(__dirname, 'src/core')
-    }
+      '@core': path.resolve(__dirname, 'src/core'),
+    },
   },
   devtool: isDev ? 'source-map' : false,
   devServer: {
     port: 3000,
-    hot: isDev
+    hot: isDev,
   },
   plugins: [
     new CleanWebpackPlugin(),
@@ -74,43 +77,44 @@ module.exports = {
       template: 'index.html',
       minify: {
         removeComments: isProd,
-        collapseWhitespace: isProd
+        collapseWhitespace: isProd,
       },
-      chunks: ['index']
+      chunks: ['index'],
     }),
     new CopyPlugin({
-      patterns: [{
-        from: path.resolve(__dirname, 'src/favicon.ico'),
-        to: path.resolve(__dirname, 'dist')
-      },
-      {
-        from: path.resolve(__dirname, 'src/img'),
-        to: path.resolve(__dirname, 'dist/img')
-      }
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist'),
+        },
+        {
+          from: path.resolve(__dirname, 'src/img'),
+          to: path.resolve(__dirname, 'dist/img'),
+        },
       ],
     }),
     new MiniCssExtractPlugin({
-      filename: filename('css')
+      filename: filename('css'),
     }),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    })
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
   ].concat(multipleHtmlPlugins),
   module: {
     rules: [
       {
         test: /\.(png|jpg)$/i,
         use: [
-            {
-              loader: 'image-webpack-loader',
-              options: {
-                bypassOnDebug: true,
-                disable: true,
-              },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
             },
-            'file-loader',
-          ],
-        },
+          },
+          'file-loader',
+        ],
+      },
       {
         test: /\.s[ac]ss$/i,
         use: [
@@ -121,15 +125,15 @@ module.exports = {
               reloadAll: true,
             },
           },
-          "css-loader",
-          "sass-loader",
+          'css-loader',
+          'sass-loader',
         ],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: jsLoaders()
-      }
+        use: jsLoaders(),
+      },
     ],
-  }
+  },
 };
