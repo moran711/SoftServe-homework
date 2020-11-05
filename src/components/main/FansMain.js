aed3191dd052e3734bc1b95a459a261e85d7de73
Roman <romaniv788@gmail.com>
2020-11-05 20:10:40 +0200

    implement fans page styles

--- src/components/main/FansMain.js
+++ src/components/main/FansMain.js
@@ -30,16 +30,50 @@ export default class FansMain {
           <div class="fan-nickname">Football fan 228</div>
         </div>
       </div>
+      <div class="appeal-container">
+        <div class="appeal-text">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+          Doloremque excepturi iusto mollitia quisquam.
+          Accusamus aperiam debitis delectus doloremque
+          dolores eius error eveniet ex harum illum ipsa ipsam
+          iusto laboriosam libero necessitatibus, obcaecati
+          optio perspiciatis possimus quod recusandae repellendus
+          repudiandae sit suscipit tempore temporibus veritatis, vero. 
+          Ad, aperiam consequatur doloremque dolores ducimus, 
+          nemo nesciunt odio officiis possimus quam qui repellat sapiente.
+        </div>
+        <div class="appeal-footer">
+          <div class="appeal-date">01.02.2020</div>
+          <div class="fan-nickname">Football fan 228</div>
+        </div>
+      </div>
+      <div class="appeal-container">
+        <div class="appeal-text">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+          Doloremque excepturi iusto mollitia quisquam.
+          Accusamus aperiam debitis delectus doloremque
+          dolores eius error eveniet ex harum illum ipsa ipsam
+          iusto laboriosam libero necessitatibus, obcaecati
+          optio perspiciatis possimus quod recusandae repellendus
+          repudiandae sit suscipit tempore temporibus veritatis, vero. 
+          Ad, aperiam consequatur doloremque dolores ducimus, 
+          nemo nesciunt odio officiis possimus quam qui repellat sapiente.
+        </div>
+        <div class="appeal-footer">
+          <div class="appeal-date">01.02.2020</div>
+          <div class="fan-nickname">Football fan 228</div>
+        </div>
+      </div>
       <form action="" class="add-appeal">
         <textarea 
           rows = "5" 
           cols = "60" 
           name = "description" 
           placeholder="Send appeal..."
-          class="add-news-text"
+          class="add-news-appeal"
           required
          ></textarea>
-        <button class="add-news-btn" type="submit">Add appeal</button>  
+        <button class="add-appeal-btn" type="submit">Add appeal</button>  
       </form>
     `;
   }
